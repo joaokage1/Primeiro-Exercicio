831577c0a7013d8599d7d5e3b7b249c7bb9f38c8
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2021-06-10 21:38:49 -0300

    Initial commit

---
+++ src/test/java/avaliacao/primeiro/exerc/cio/PrimeiroExercicioApplicationTests.java
@@ -0,0 +1,56 @@
+package avaliacao.primeiro.exerc.cio;
+
+import avaliacao.primeiro.exercicio.PrimeiroExercicioApplication;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrimeiroExercicioApplicationTests {
+
+	static PrimeiroExercicioApplication app;
+
+	@BeforeAll
+	static void initialize(){
+		app = new PrimeiroExercicioApplication();
+	}
+
+	@Test
+	void testValoresOk() {
+
+		int solution = app.solution(213);
+		assertEquals(321, solution);
+
+		solution = app.solution(555);
+		assertEquals(555, solution);
+
+		solution = app.solution(456789);
+		assertEquals(987654, solution);
+
+		solution = app.solution(665919);
+		assertEquals(996651, solution);
+
+		solution = app.solution(100000000);
+		assertEquals(100000000, solution);
+	}
+
+	@Test
+	void testValoresNOk() {
+
+		int solution = app.solution(213000000);
+		assertEquals(-1, solution);
+
+		solution = app.solution(200000000);
+		assertEquals(-1, solution);
+
+		solution = app.solution(300000000);
+		assertEquals(-1, solution);
+
+	}
+
+	@Test
+	void testNumberFormatException(){
+		int solution = app.solution(1000020002);
+		assertEquals(-1, solution);
+	}
+}
